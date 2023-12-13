fd85ca5338fdce8fb6afdfdaf69544a74db0b303
Priscilla-22 <priscillamw123@gmail.com>
2023-12-13 23:29:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob' ;
+
+const leastFavoriteCustomer = 'Jane'
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+console.log(upperCaseCustomerName());
+
+
+function setBestCustomer() {
+    return bestCustomer;
+}
+console.log(setBestCustomer());
+
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+overwriteBestCustomer('maybe bob')
+console.log(bestCustomer);
+
+
+function changeLeastFavoriteCustomer(newLeastFavourite) {
+    leastFavoriteCustomer = newLeastFavourite;
+}
+changeLeastFavoriteCustomer('Leishan');
+console.log(leastFavoriteCustomer);
